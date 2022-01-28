4ad6d87f23bd437892dfa6926b61ebe6f3956451
Rylee George <ryleegeorge1@gmail.com>
2022-01-28 23:05:26 +1100

    Added drive format

--- setup.sh
+++ setup.sh
@@ -34,9 +34,8 @@ print () {
 }
 
 
-print "Weclome. Arch will now be setup"
+print "Weclome to the Hephaestus arch installer"
 
-print "Welcome to easy-arch, a script made in order to simplify the process of installing Arch Linux."
 PS3="Please select the disk where Arch Linux is going to be installed: "
 select ENTRY in $(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd");
 do
@@ -45,6 +44,38 @@ do
     break
 done
 
+# Deleting old partition scheme.
+read -r -p "This will delete the current partition table on $DISK. Do you agree [y/N]? " response
+response=${response,,}
+if [[ "$response" =~ ^(yes|y)$ ]]; then
+    print "Wiping $DISK."
+    wipefs -af "$DISK" &>/dev/null
+    sgdisk -Zo "$DISK" &>/dev/null
+else
+    print "Quitting."
+    exit
+fi
 
+# Creating a new partition scheme.
+print "Creating the partitions on $DISK."
+parted -s "$DISK" \
+    mklabel gpt \
+    mkpart ESP fat32 1MiB 513MiB \
+    set 1 esp on \
+    mkpart ROOT 513MiB 100% \
 
+ESP="/dev/disk/by-partlabel/ESP"
+ROOT="/dev/disk/by-partlabel/ROOT"
 
+# Informing the Kernel of the changes.
+print "Informing the Kernel about the disk changes."
+partprobe "$DISK"
+
+# Formatting the ESP as FAT32.
+print "Formatting the EFI Partition as FAT32."
+mkfs.fat -F 32 $ESP &>/dev/null
+
+# Formatting the ROOT as EXT4.
+print "Formatting the ROOT Partition as EXT4."
+mkfs.ext4 $ROOT &>/dev/null
+mount $ROOT /mnt
