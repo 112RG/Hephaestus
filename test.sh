85c6deedaff6fb626caca8251708ed5b61f17a91
Rylee George <ryleegeorge1@gmail.com>
2022-01-29 02:13:49 +1100

    Add test script

---
+++ test.sh
@@ -0,0 +1,31 @@
+PS3="Please select the disk where Arch Linux is going to be installed: "
+select ENTRY in $(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd");
+do
+    DISK=$ENTRY
+    print "Installing Arch Linux on $DISK."
+    break
+done
+EFI_PARTITION="${DISK}1"
+ROOT_PARTITION="${DISK}2"
+# disk prep
+sgdisk -Z ${DISK} # zap all on disk
+sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+# create partitions
+sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
+sgdisk -n 2:0:0     ${DISK} # partition 2 (Root), default start, remaining
+# set partition types
+sgdisk -t 1:ef00 ${DISK}
+sgdisk -t 2:8300 ${DISK}
+# label partitions
+sgdisk -c 1:"UEFISYS" ${DISK}
+sgdisk -c 2:"ROOT" ${DISK}
+# make filesystems
+echo -e "\nCreating Filesystems...\n$HR"
+mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
+mkfs.ext4 -L "ROOT" "${DISK}1"
+# mount target
+mkdir /mnt
+mount "${ROOT_PARTITION}" /mnt
+mkdir /mnt/boot
+mkdir /mnt/boot/efi
+mount "${EFI_PARTITION}" /mnt/boot/
